6341efd986c960effee4e00157417b67198b13c8
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-04-25 22:02:49 +0300

    Update config validation

--- src/getConfig.ts
+++ src/getConfig.ts
@@ -5,7 +5,7 @@ import path from "path";
 export const getConfig: GetConfigFunction = async () => {
     const config = JSON.parse((await fs.readFile(path.join(__dirname, "config.json"))).toString()) as Config;
     if (!Object.keys(config).every((x: keyof Config) => typeof config[x] !== "undefined")) {
-        throw new Error("Invalid app config! Please check config.json for the propertis: `numberOfPasswords`, `length`");
+        throw new Error("Invalid app config! Please check config.json for the propertis: `numberOfPasswords`, `length`, `storageType`");
     }
     if (config.length < 1) {
         throw new Error("Invalid app config! `length` should be at least 1");
