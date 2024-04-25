51f69124a18713290d691b1619e8f064ca23941f
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-04-25 22:06:21 +0300

    Update config validation

--- src/getConfig.ts
+++ src/getConfig.ts
@@ -1,10 +1,16 @@
-import { Config, GetConfigFunction } from "./types";
+import { Config, GetConfigFunction, StorageType } from "./types";
 import fs from "fs/promises";
 import path from "path";
 
+const baseConfig: Config = Object.freeze({
+    length: 0,
+    storageType: StorageType.ConsoleOnly,
+    numberOfPasswords: 0
+});
+
 export const getConfig: GetConfigFunction = async () => {
     const config = JSON.parse((await fs.readFile(path.join(__dirname, "config.json"))).toString()) as Config;
-    if (!Object.keys(config).every((x: keyof Config) => typeof config[x] !== "undefined")) {
+    if (!Object.keys(baseConfig).every((x: keyof Config) => typeof config[x] !== "undefined")) {
         throw new Error("Invalid app config! Please check config.json for the propertis: `numberOfPasswords`, `length`, `storageType`");
     }
     if (config.length < 1) {
